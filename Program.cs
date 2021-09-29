91fbf9275fb2a421e6c392790214949bd3fb5f80
kavallo <luis.aneudy@gmail.com>
2021-09-29 14:28:47 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
