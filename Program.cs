57ee18d83626e90ef0ce55a28e1758a8b315f6b9
kavallo <luis.aneudy@gmail.com>
2021-09-29 14:32:31 -0400

    Prbando los Test

--- Program.cs
+++ Program.cs
@@ -2,11 +2,16 @@
 
 namespace ConsoleApp4
 {
-    class Program
+    public class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+        }
+
+        public static bool autenticado()
+        {
+            return true;
         }
     }
 }
